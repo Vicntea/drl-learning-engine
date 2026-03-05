ce205eea7f52842086d61c2ee1633987bc041295
Vicente Alves <loshaywinover@gmail.com>
2026-03-04 21:43:03 -0300

    new(Initialize): init repo

---
+++ frontend/src/pages/Nodo1aPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nodo1aScorePlayer from "../components/Nodo1aScorePlayer";
+
+const Nodo1aPage: React.FC = () => (
+  <div style={{ maxWidth: 600, margin: "0 auto", padding: 24 }}>
+    <h2>Ejercicio Nodo1a</h2>
+    <Nodo1aScorePlayer />
+  </div>
+);
+
+export default Nodo1aPage;
